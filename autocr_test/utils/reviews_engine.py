25d5e8ce5bca2a6445800da2f8ded02846c558e4
armen.akopyan <armen.akopyan@marakas.digital>
2024-12-07 19:31:32 +0000

    better comments

--- autocr_test/utils/reviews_engine.py
+++ autocr_test/utils/reviews_engine.py
@@ -18,8 +18,8 @@ def convert_from_repo_file_to_file_for_review(file: RepoFileModel) -> FileForRev
     )
 
 def get_review(repo_files_data: list[RepoFileModel], assignment_description : str, candidate_level: RepoFileType) -> str:
-    code_data_to_send = [convert_from_repo_file_to_file_for_review(x) for x in repo_files_data]
-    code_data_to_send_json = [x.model_dump_json(exclude_none=True) for x in code_data_to_send] # exclude none values to save tokens
+    code_data_to_send = [convert_from_repo_file_to_file_for_review(x) for x in repo_files_data] # convert to reduce size
+    code_data_to_send_json = [x.model_dump_json(exclude_none=True) for x in code_data_to_send] # exclude none for same reason
     code_data_to_send_dump = json.dumps(code_data_to_send_json)
 
     stream = client.chat.completions.create(
@@ -27,7 +27,7 @@ def get_review(repo_files_data: list[RepoFileModel], assignment_description : st
         messages=[{
             "role": "user",
             "content": PROMPT.format(
-                CANDIDATE_LEVEL=candidate_level, ASSIGNMENT_DESCRIPTION=assignment_description , 
+                CANDIDATE_LEVEL=candidate_level, ASSIGNMENT_DESCRIPTION=assignment_description, 
                 REPO_DUMP=code_data_to_send_dump
             )
         }],
