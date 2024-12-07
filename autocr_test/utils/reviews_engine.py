8a0d7a55f9efb52241d28e1b58779926b1cc2f82
armen.akopyan <armen.akopyan@marakas.digital>
2024-12-07 15:32:07 +0000

    better get_review

--- autocr_test/utils/reviews_engine.py
+++ autocr_test/utils/reviews_engine.py
@@ -17,25 +17,21 @@ def convert_from_repo_file_to_file_for_review(file: RepoFileModel) -> FileForRev
         included_files=[convert_from_repo_file_to_file_for_review(x) for x in file.files] if file.files else None
     )
 
-def get_review(repo_files_data: list[RepoFileModel], assignement_description: str, candidate_level: RepoFileType) -> str:
-    code_data_to_send: list[FileForReview] = []
-
-    for file in repo_files_data:
-        code_data_to_send.append(convert_from_repo_file_to_file_for_review(file))
-    
-    code_data_to_send_raw = [x.model_dump_json(exclude_none=True) for x in code_data_to_send]
-    code_data_to_send_raw = json.dumps(code_data_to_send_raw)
+def get_review(repo_files_data: list[RepoFileModel], assignment_description : str, candidate_level: RepoFileType) -> str:
+    code_data_to_send = [convert_from_repo_file_to_file_for_review(x) for x in repo_files_data]
+    code_data_to_send_json = [x.model_dump_json(exclude_none=True) for x in code_data_to_send] # exclude none values to save tokens
+    code_data_to_send_dump = json.dumps(code_data_to_send_json)
 
     stream = client.chat.completions.create(
-        model="gpt-4o-mini",
+        model=config.OPENAI_MODEL,
         messages=[{
             "role": "user",
             "content": PROMPT.format(
-                CANDIDATE_LEVEL=candidate_level, ASSIGNEMENT_DESCRIPTION=assignement_description, 
-                REPO_DUMP=code_data_to_send_raw
+                CANDIDATE_LEVEL=candidate_level, ASSIGNMENT_DESCRIPTION=assignment_description , 
+                REPO_DUMP=code_data_to_send_dump
             )
         }],
-        stream=True,
+        stream=True,  # stream to not block the event loop
     )
 
     review = ''
