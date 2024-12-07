aa36513106cec454dc85d47285cf0e8ad018e7b0
armen.akopyan <armen.akopyan@marakas.digital>
2024-12-07 15:14:47 +0000

    upd

--- autocr_test/utils/constants.py
+++ autocr_test/utils/constants.py
@@ -1,7 +1,7 @@
 PROMPT="""
 Make the code review of provided dump of github repository.
 Notice, that the code author level is {CANDIDATE_LEVEL} and the assignment description is "{ASSIGNEMENT_DESCRIPTION}".
-The review must include this parts: 1. Analyzed files list. 2. Downsides/comments. 3. Rating from 1 to 5. 4. Conclusion.
+The review must include this parts: 1. Analyzed files list. 2. Downsides/advantages/annotations. 3. Rating from 1 to 5. 4. Conclusion.
 Here is the dump of the repository:
 {REPO_DUMP}
 """
